1be64c05520471dd5879ab232c889f630a2b72eb
The Anh <dotheanh12399@gmail.com>
2022-07-14 02:08:58 +0700

    Add FAB

--- index.js
+++ index.js
@@ -10,34 +10,34 @@ $(document).ready(function () {
     // }, 20);
 
 
+
     ///////////// COUNT DOWN TO NEW VERSION
     let daysItem = document.querySelector("#cdv-days");
     let hoursItem = document.querySelector("#cdv-hours");
     let minItem = document.querySelector("#cdv-min");
     let secItem = document.querySelector("#cdv-sec");
     let countDown = () => {
-      let futureDate = getNextReleaseTime();
-      let currentDate = new Date();
-      let myDate = futureDate - currentDate;
-    
-      let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
-      let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
-      let min = Math.floor(myDate / 1000 / 60) % 60;
-      let sec = Math.floor(myDate / 1000) % 60;
-    
-      daysItem.innerHTML = days;
-      hoursItem.innerHTML = hours;
-      minItem.innerHTML = min;
-      secItem.innerHTML = sec;
+        let futureDate = getNextReleaseTime();
+        let currentDate = new Date();
+        let myDate = futureDate - currentDate;
+
+        let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+        let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+        let min = Math.floor(myDate / 1000 / 60) % 60;
+        let sec = Math.floor(myDate / 1000) % 60;
+
+        daysItem.innerHTML = days;
+        hoursItem.innerHTML = hours;
+        minItem.innerHTML = min;
+        secItem.innerHTML = sec;
     }
     countDown();
     setInterval(countDown, 1000);
 });
 
-function getNextReleaseTime()
-{
+function getNextReleaseTime() {
     const currentDate = new Date();
-    let nextDate = currentDate.setHours(0,0,0,1);
+    let nextDate = currentDate.setHours(0, 0, 0, 1);
     nextDate = currentDate.setDate(currentDate.getDate() + (7 - currentDate.getDay()));
     return nextDate;
 }
