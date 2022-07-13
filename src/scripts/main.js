63ebb63220d8819c28b520760586896cb9060d82
The Anh <dotheanh12399@gmail.com>
2022-07-14 02:45:05 +0700

    FAB clock progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
-import LocomotiveScroll from '../../../src/locomotive-scroll';
 
 (function() {
-
+    $("#fab-container").css("visibility", "hidden");
     document.documentElement.classList.add('is-loaded');
     document.documentElement.classList.remove('is-loading');
 
@@ -50,6 +49,23 @@ import LocomotiveScroll from '../../../src/locomotive-scroll';
 
             document.documentElement.setAttribute('data-direction', instance.direction)
 
+            // DISPLAY FAB PROGRESS
+            const NO_CLOCK_OFFSET = 20; // ở đoạn đầu trang web thì ko hiện FAB đồng hồ và ko tính progress
+            const clockProgress = (progress - NO_CLOCK_OFFSET)*360/(360 - NO_CLOCK_OFFSET);
+            if (clockProgress < 0) {
+                $("#fab-container").css("visibility", "hidden");
+            }
+            else {
+                $("#fab-container").css("visibility", "visible");
+            }
+            let hour = (clockProgress);
+            let minus = (hour*12);
+            let second = (minus*60);
+
+            // 0, -80, 35 là để chỉnh các kim về vị trí 0 giờ
+            $("#fab-clock .clock-hand-hour").css("transform",`rotate(`+(0+hour)+`deg)`);
+            $("#fab-clock .clock-hand-minus").css("transform",`rotate(`+(-80+minus)+`deg)`);
+            $("#fab-clock .clock-hand-second").css("transform",`rotate(`+(35+second)+`deg)`);
         });
 
         scroll.on('call', (value, way, obj) => {
