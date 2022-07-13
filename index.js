483581bc8d69add1b45855beb0e167cd50b955af
The Anh <dotheanh12399@gmail.com>
2022-07-13 22:09:57 +0700

    Try scroll

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // let i = 10;
+    // let int = setInterval(function () {
+    //     window.scrollTo({
+    //         top: i,
+    //         behavior: 'smooth'
+    //     });
+    //     i += 10;
+    //     if (i >= document.body.scrollHeight) clearInterval(int);
+    // }, 20);
+});
